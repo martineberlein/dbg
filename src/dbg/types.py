e5ed729a2fb7310a50ae444ec223ca5872752c0f
Martin Eberlein <33397273+martineberlein@users.noreply.github.com>
2025-02-12 14:34:33 +0100

    Add InputExplainer and HypothesisBasedExplainer classes for input feature debugging

--- src/dbg/types.py
+++ src/dbg/types.py
@@ -3,5 +3,7 @@ from typing import Callable, Union
 from dbg.data.input import Input
 from dbg.data.oracle import OracleResult
 
-OracleType = Callable[[Union[Input, str]], OracleResult]
+SingleOracleType = Callable[[Union[Input, str]], OracleResult]
 BatchOracleType = Callable[[Union[set[Input], set[str]]], dict[Input, OracleResult]]
+
+OracleType = Union[SingleOracleType, BatchOracleType]
