f2f5ba0e45a93d8cf09dc0d3296cf07c8c8c7b91
Martin Eberlein <schuldner-01.vordach@icloud.com>
2025-03-10 20:18:13 +0100

    Add AlhazenInput class and update Alhazen to integrate learner and generator

---
+++ alhazen/__init__.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+from dbg.data.input import Input
+from dbg.data.oracle import OracleResult
+
+
+class AlhazenInput(Input):
+    def traverse(self):
+        pass
+
+    def __hash__(self) -> int:
+        pass
+
+    @classmethod
+    def from_str(cls, grammar, input_string, oracle: Optional[OracleResult] = None):
+        pass
+
+
+Grammar = dict[str, list[str]]
--- alhazen/alhazen.py
+++ alhazen/alhazen.py
@@ -1,13 +1,34 @@
-from typing import Set
+from typing import Set, Union, Iterable
 
 from dbg.core import HypothesisBasedExplainer
 from dbg.data.input import Input
 from dbg.explanation.candidate import ExplanationSet
+from dbg.types import OracleType
+
+from alhazen._learner import AlhazenLearner
+from alhazen._generator import AlhazenGenerator
+from alhazen import Grammar
 
 
 class Alhazen(HypothesisBasedExplainer):
+
+    def __init__(self, grammar: Grammar, oracle: OracleType,
+                 initial_inputs: Union[Iterable[str], Iterable[Input]],
+                 **kwargs):
+
+        learner = AlhazenLearner()
+        generator = AlhazenGenerator(grammar)
+        super().__init__(grammar, oracle, initial_inputs, learner, generator, **kwargs)
+
     def learn_candidates(self, test_inputs: Set[Input]) -> ExplanationSet:
         pass
 
     def generate_test_inputs(self, candidates: ExplanationSet) -> Set[Input]:
         pass
+
+
+
+if __name__ == "__main__":
+
+
+    alhazen = Alhazen()
