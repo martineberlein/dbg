a69dd7eb338914caaf221e621ae0153ad746eb03
Martin Eberlein <schuldner-01.vordach@icloud.com>
2025-03-11 12:43:22 +0100

    Refactor Alhazen class: simplify type hints and remove unused methods

--- alhazen/core.py
+++ alhazen/core.py
@@ -1,7 +1,7 @@
-from typing import Set, Union, Iterable, Optional
+from typing import Set, Iterable
 
 from dbg.core import HypothesisBasedExplainer
-from dbg.data.input import Input, OracleResult
+from dbg.data.input import OracleResult
 from dbg.explanation.candidate import ExplanationSet
 from dbg.types import OracleType
 
@@ -15,7 +15,7 @@ from alhazen.features.collector import GrammarFeatureCollector
 class Alhazen(HypothesisBasedExplainer):
 
     def __init__(self, grammar: Grammar, oracle: OracleType,
-                 initial_inputs: Union[Iterable[str], Iterable[Input]],
+                 initial_inputs: Iterable[str],
                  **kwargs):
 
         learner = AlhazenLearner()
@@ -24,7 +24,7 @@ class Alhazen(HypothesisBasedExplainer):
 
         self.collector = GrammarFeatureCollector(grammar)
 
-    def set_initial_inputs(self, test_inputs: Iterable[str]) -> set[Input]:
+    def set_initial_inputs(self, test_inputs: Iterable[str]) -> set[AlhazenInput]:
         return {AlhazenInput.from_str(self.grammar, inp, self.oracle(inp)) for inp in test_inputs}
 
     def prepare_test_inputs(self, test_inputs: set[AlhazenInput]) -> Set[AlhazenInput]:
@@ -35,51 +35,8 @@ class Alhazen(HypothesisBasedExplainer):
     def learn_candidates(self, test_inputs: Set[AlhazenInput]) -> ExplanationSet:
         print("Learning candidates")
         return self.learner.learn_explanation(test_inputs)
-        pass
 
-    def generate_test_inputs(self, candidates: ExplanationSet) -> Set[Input]:
-        pass
-
-    def run_test_inputs(self, test_inputs: Set[Input]) -> Set[Input]:
-        pass
-
-
-
-if __name__ == "__main__":
-    from fuzzingbook.GrammarFuzzer import is_valid_grammar
-    import math
-
-    def oracle(inp: AlhazenInput | str) -> OracleResult:
-        try:
-            eval(str(inp), {"sqrt": math.sqrt, "sin": math.sin, "cos": math.cos, "tan": math.tan})
-            return OracleResult.PASSING
-        except ValueError:
-            return OracleResult.FAILING
-
-    grammar_alhazen = {
-        "<start>": ["<arith_expr>"],
-        "<arith_expr>": ["<function>(<number>)"],
-        "<function>": ["sqrt", "sin", "cos", "tan"],
-        "<number>": ["<maybe_minus><onenine><maybe_digits><maybe_frac>"],
-        "<maybe_minus>": ["", "-"],
-        "<onenine>": [str(num) for num in range(1, 10)],
-        "<digit>": [str(num) for num in range(0, 10)],
-        "<maybe_digits>": ["", "<digits>"],
-        "<digits>": ["<digit>", "<digit><digits>"],
-        "<maybe_frac>": ["", ".<digits>"],
-    }
-    is_valid_grammar(grammar_alhazen)
-
-    from fuzzingbook.GrammarFuzzer import GrammarFuzzer
-
-    initial_inputs = ["sqrt(-900)", "sin(-3)", "cos(10)", "tan(5)"]
-    for _ in range(100):
-        initial_inputs.append(GrammarFuzzer(grammar_alhazen).fuzz())
-
-    alhazen = Alhazen(
-        grammar=grammar_alhazen,
-        initial_inputs=initial_inputs,
-        oracle=oracle,
-    )
-
-    diagnosis = alhazen.explain()
+    def generate_test_inputs(self, explanations: ExplanationSet) -> Set[AlhazenInput]:
+        print(f"Generating new test inputs for {len(explanations)} hypotheses.")
+        test_inputs = self.engine.generate(explanations=explanations)
+        return test_inputs
