58dd882db18f4c9c0b66140e25a612c7376e6e88
Martin Eberlein <schuldner-01.vordach@icloud.com>
2025-03-11 12:44:12 +0100

    Add AlhazenGenerator class: implement initialization and generation logic using GrammarFuzzer

--- alhazen/_generator.py
+++ alhazen/_generator.py
@@ -1,6 +1,16 @@
+from fuzzingbook.GrammarFuzzer import GrammarFuzzer
+
 from dbg.generator.generator import Generator
+
+from alhazen import Grammar
+from alhazen._data import AlhazenInput
 
 
 class AlhazenGenerator(Generator):
+
+    def __init__(self, grammar: Grammar, **kwargs):
+        super().__init__(grammar, **kwargs)
+        self.fuzzer = GrammarFuzzer(grammar)
+
     def generate(self, *args, **kwargs):
-        pass
+        return AlhazenInput(self.fuzzer.fuzz_tree())
