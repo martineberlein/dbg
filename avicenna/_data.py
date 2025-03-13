34570d72d67f3312a442a0a577c785710eda9185
Martin Eberlein <33397273+martineberlein@users.noreply.github.com>
2025-03-13 14:32:01 +0100

    Add AvicennaInput class with feature handling and parsing capabilities

--- avicenna/_data.py
+++ avicenna/_data.py
@@ -3,11 +3,39 @@ from typing import Optional
 from dbg.data.input import Input
 from dbg.data.oracle import OracleResult
 
+from avicenna import DerivationTree, EarleyParser
+from avicenna.features.features import FeatureVector
+
 
 class AvicennaInput(Input):
+    """
+    Class describing a test input.
+    """
+
+    def __init__(self, tree: DerivationTree, oracle: OracleResult = None):
+        super().__init__(tree, oracle)
+        self.__features: Optional[FeatureVector] = None
+
+    @property
+    def features(self) -> FeatureVector:
+        return self.__features
+
+    @features.setter
+    def features(self, features_: FeatureVector):
+        self.__features = features_
+
+    def update_features(self, features_: FeatureVector) -> "Input":
+        self.__features = features_
+        return self
+
     def __hash__(self) -> int:
-        pass
+        return self.__tree.structural_hash()
 
     @classmethod
     def from_str(cls, grammar, input_string, oracle: Optional[OracleResult] = None):
-        pass
+        return cls(
+            DerivationTree.from_parse_tree(
+                next(EarleyParser(grammar).parse(input_string))
+            ),
+            oracle,
+        )
